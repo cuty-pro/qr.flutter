57bcf02e9be5aa0fb2d78f96687060b6c0b960b0
Stanislav Ilin <youngfrezyx@gmail.com>
2024-02-18 12:06:01 +0500

    Update example

--- example/lib/main_screen.dart
+++ example/lib/main_screen.dart
@@ -20,9 +20,7 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    const String message =
-        // ignore: lines_longer_than_80_chars
-        'Hey this is a QR code. Change this value in the main_screen.dart file.';
+    const String message = 'https://links.cuty.pro/master-share';
 
     final FutureBuilder<ui.Image> qrFutureBuilder = FutureBuilder<ui.Image>(
       future: _loadOverlayImage(),
@@ -37,12 +35,14 @@ class _MainScreenState extends State<MainScreen> {
             data: message,
             version: QrVersions.auto,
             eyeStyle: const QrEyeStyle(
-              eyeShape: QrEyeShape.square,
-              color: Color(0xff128760),
+              eyeShape: QrEyeShape.radius,
+              color: ui.Color.fromARGB(255, 0, 0, 0),
+              radius: 15,
             ),
             dataModuleStyle: const QrDataModuleStyle(
-              dataModuleShape: QrDataModuleShape.circle,
-              color: Color(0xff1a5441),
+              dataModuleShape: QrDataModuleShape.radius,
+              color: Color(0xFFE5017C),
+              radius: 2,
             ),
             // size: 320.0,
             embeddedImage: snapshot.data,
@@ -80,8 +80,7 @@ class _MainScreenState extends State<MainScreen> {
 
   Future<ui.Image> _loadOverlayImage() async {
     final Completer<ui.Image> completer = Completer<ui.Image>();
-    final ByteData byteData =
-        await rootBundle.load('assets/images/4.0x/logo_yakka.png');
+    final ByteData byteData = await rootBundle.load('assets/logo_2.png');
     ui.decodeImageFromList(byteData.buffer.asUint8List(), completer.complete);
     return completer.future;
   }
